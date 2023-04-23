720a29e2b087f464c7be93b2769185eca9d3e2f6
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2023-04-23 19:19:56 +0300

    Add tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div class="app">Nuxt app</div>
 </template>
+
+<style lang="postcss">
+.app {
+  @apply p-4 text-primary font-bold;
+}
+</style>
